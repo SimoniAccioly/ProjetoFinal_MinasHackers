f464f5e40378827ddd6c35eb4a9e64578ccb2dc3
SimoniAccioly <67556668+SimoniAccioly@users.noreply.github.com>
2020-12-10 23:37:16 -0300

    atualizando as funções do search

--- backEnd/src/controller/search-controller.js
+++ backEnd/src/controller/search-controller.js
@@ -1,121 +1,36 @@
 const eventsCollection = require("../models/eventSchema");
 
-const findForCategory= (request, response) => {
-    const category = request.params.category;
-  
-   eventsCollection.find({ category: category }, (error, event) => {
-      console.log(event);
-      if (error) {
-        response.status(500).send(error);
-      } else if (event == "") {
-        return response.status(400).send("Categoria não encontrado");
-      } else {
-        return response.status(200).send({
-          message: "Categoria encontrada!",
-          event,
-        });
-      }
-    });
-  };
+const findForCategory = (request, response) => {
+  console.log(request.params);
+  const filter = {};
+  if (request.query.category) filter.category = request.query.category;
 
-  const findForName= (request, response) => {
-    const name = request.params.eventName;
-  
-   eventsCollection.find({ eventName: name}, (error, event) => {
-      console.log(event);
-      if (error) {
-        response.status(500).send(error);
-      } else if (event == "") {
-        return response.status(400).send("Evento não encontrado");
-      } else {
-        return response.status(200).send({
-          message: "Evento não encontrado!",
-          event,
-        });
-      }
-    });
-  };
+  if (request.query.eventName) filter.eventName = request.query.eventName;
 
-  const findForDate= (request, response) => {
-    const date = request.params.eventDate;
-  
-   eventsCollection.find({ eventDate: date}, (error, event) => {
-      console.log(event);
-      if (error) {
-        response.status(500).send(error);
-      } else if (event == "") {
-        return response.status(400).send("Evento não encontrado");
-      } else {
-        return response.status(200).send({
-          message: "Evento não encontrado!",
-          event,
-        });
-      }
-    });
-  };
-  
+  if (request.query.eventDate) filter.eventDate = request.query.eventDate;
 
-  const findForTheme= (request, response) => {
-    const theme = request.params.mainTheme;
-  
-   eventsCollection.find({ mainTheme: theme}, (error, event) => {
-      console.log(event);
-      if (error) {
-        response.status(500).send(error);
-      } else if (event == "") {
-        return response.status(400).send("Evento não encontrado");
-      } else {
-        return response.status(200).send({
-          message: "Evento não encontrado!",
-          event,
-        });
-      }
-    });
-  };
+  if (request.query.mainTheme) filter.mainTheme = request.query.mainTheme;
 
-  const findForCost= (request, response) => {
-    const costs = request.params.costs;
-  
-   eventsCollection.find({ costs: costs}, (error, event) => {
-      console.log(event);
-      if (error) {
-        response.status(500).send(error);
-      } else if (event == "") {
-        return response.status(400).send("Evento não encontrado");
-      } else {
-        return response.status(200).send({
-          message: "Evento não encontrado!",
-          event,
-        });
-      }
-    });
-  };
+  if (request.query.costs) filter.costs = request.query.costs;
 
-  const findForSubcategory= (request, response) => {
-    const subcategory = request.params.subcategory;
-  
-   eventsCollection.find({ subcategory: subcategory}, (error, event) => {
-      console.log(event);
-      if (error) {
-        response.status(500).send(error);
-      } else if (event == "") {
-        return response.status(400).send("Evento não encontrado");
-      } else {
-        return response.status(200).send({
-          message: "Evento não encontrado!",
-          event,
-        });
-      }
-    });
-  };
+  if (request.query.subCategory) filter.subCategory = request.query.subCategory;
 
+  console.log(filter);
+  eventsCollection.find(filter, (error, event) => {
+    console.log(event);
+    if (error) {
+      response.status(500).send(error);
+    } else if (event == "") {
+      return response.status(400).send("Categoria não encontrado");
+    } else {
+      return response.status(200).send({
+        message: "Categoria encontrada!",
+        event,
+      });
+    }
+  });
+};
 
-  module.exports = {
-    findForCategory,
-    findForName,
-    findForDate,
-    findForTheme,
-    findForCost,
-    findForSubcategory
-  };
-  
+module.exports = {
+  findForCategory,
+};
--- backEnd/src/routes/search-route.js
+++ backEnd/src/routes/search-route.js
@@ -3,12 +3,8 @@ const router = express.Router()
 const controller = require("../controller/search-controller")
 
 
-router.get('/:category', controller.findForCategory)
-router.get('/:name', controller.findForCategory)
-router.get('/:date', controller.findForDate)
-router.get('/:theme', controller.findForTheme)
-router.get('/:cost', controller.findForCost)
-router.get('/:subcategory', controller.findForSubcategory)
+router.get('/', controller.findForCategory)
+
 
 
 module.exports = router
